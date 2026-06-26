87f8733c26ef76e478396955124f85c278af1622
Felipe (TOK) <h4shi.supply@gmail.com>
2026-06-26 07:46:38 -0300

    Hide account network icon from assistive tech

--- components/app-account-bar.tsx
+++ components/app-account-bar.tsx
@@ -168,7 +168,7 @@ export function AppAccountBar({
                         variant="outline"
                         size="sm"
                         className="w-full justify-start"
-                        icon={<RefreshCw className="h-4 w-4" />}
+                        icon={<RefreshCw className="h-4 w-4" aria-hidden="true" />}
                         title={isMiniPay ? dictionary.actions.refreshNetwork : dictionary.actions.switchNetwork}
                         onClick={() => {
                           setIsMenuOpen(false);
